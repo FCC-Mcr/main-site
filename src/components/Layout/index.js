a04f36e33bac3f51dba0af12e0b16d14583d7425
j <someboredlad@hotmail.com>
2019-08-03 14:31:39 +0100

    feature: starter css configs & demo header and styling
    feature: starter css configs & demo header and styling
    
    feature: starter css configs & demo header and styling

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header/"
-import "./layout.css"
+
+import './layout.scss'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,14 +27,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <div>
         <main>{children}</main>
       </div>
     </>
