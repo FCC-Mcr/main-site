5157922c4a4d415a44c74a1c5218fa8a488e44bb
Adam <adamcollierart@gmail.com>
2019-08-09 18:44:32 +0100

    feat: prettier runs pre commit (#10)

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -3,30 +3,29 @@ import PropTypes from "prop-types"
 import React from "react"
 
 // import Image from '../../Image/'
-import styles from './header.module.scss'
+import styles from "./header.module.scss"
 
 const MainNav = () => (
   <nav>
-  <a href="https://www.freecodecamp.org/forum/c/united-kingdom-study-groups/manchester">Forum</a>
-  <Link to="/">Organisers</Link>
-  <Link to="/blog/">Blog</Link>
-  <Link to="/learning-resources/">Learning Resources</Link>
-</nav>
-);
-
+    <a href="https://www.freecodecamp.org/forum/c/united-kingdom-study-groups/manchester">
+      Forum
+    </a>
+    <Link to="/">Organisers</Link>
+    <Link to="/blog/">Blog</Link>
+    <Link to="/learning-resources/">Learning Resources</Link>
+  </nav>
+)
 
 const Header = ({ siteTitle }) => (
   <header className={`${styles.header}  u-shadow--2`}>
-      
-      <h2>
-        <Link to="/" className={styles.title}>
-          {/* need to bring in icon */}
-          {siteTitle}
-        </Link>
-      </h2>
+    <h2>
+      <Link to="/" className={styles.title}>
+        {/* need to bring in icon */}
+        {siteTitle}
+      </Link>
+    </h2>
 
-      <MainNav />
-
+    <MainNav />
   </header>
 )
 
