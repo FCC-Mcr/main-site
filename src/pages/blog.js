4e754a29ecadbcd557e7c989394beeb47cdb21da
Adam Collier <adamcollierart@gmail.com>
2019-08-10 11:35:31 +0100

    style: adjusted blog post spacing

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
+import styles from "./blog.module.scss"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
@@ -8,17 +9,14 @@ const BlogPage = ({ data }) => (
   <Layout blog>
     <SEO title="Blog" />
     <h1>Blog</h1>
-    <p>
-      Welcome to the FCC Blog, writtten and curated by members of the community
-    </p>
     {data.allMarkdownRemark.edges.map(({ node }) => (
-      <div key={node.id}>
+      <div className={styles.blogPost} key={node.id}>
         <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
           <h3>{node.frontmatter.title}</h3>
         </Link>
         <p>{node.excerpt}</p>
-        <p>Written by {node.frontmatter.author}</p>
-        <p>{node.timeToRead} min read</p>
+        <small>Written by {node.frontmatter.author}</small>
+        <small>{node.timeToRead} min read</small>
       </div>
     ))}
   </Layout>
