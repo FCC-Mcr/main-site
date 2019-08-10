271484dd73755fcd17510da50320e13cd17ef641
Adam Collier <adamcollierart@gmail.com>
2019-08-10 12:13:40 +0100

    feat: added an svg example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Station from "../icons/station.svg"
 
 // components
 import Layout from "../components/Layout/"
@@ -7,6 +8,7 @@ import SEO from "../components/SEO/"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Station />
     Ooh a big empty home page!!!
   </Layout>
 )
