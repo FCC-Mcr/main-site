a252a6c040847c700743a05cdf8831d1cf3e7b6f
Adam Collier <adamcollierart@gmail.com>
2019-08-11 20:40:09 +0100

    feat: time added WIP button

--- src/components/NextCard/index.js
+++ src/components/NextCard/index.js
@@ -1,15 +1,42 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import styles from "./nextcard.module.scss"
 
 import Clock from "../../icons/clock.svg"
 import Location from "../../icons/location.svg"
 
-const index = props => {
+const index = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMeetup(filter: { title: { regex: "/FreeCodeCamp/" } }, limit: 1) {
+        edges {
+          node {
+            id
+            title
+            description
+            location
+            start
+            end
+            calendarId
+          }
+        }
+      }
+    }
+  `)
+  console.log(data)
+
+  const date = new Date(data.allMeetup.edges[0].node.start)
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  }
+
   return (
     <div className={styles.card}>
       <p>Our next meet up is</p>
-      <h2>Wednesday 25th September</h2>
+      <h2>{date.toLocaleDateString("en-GB", options)}</h2>
       <div>
         <p>
           <Clock />
@@ -26,3 +53,24 @@ const index = props => {
 }
 
 export default index
+
+export const query = graphql`
+  query {
+    FreeCodeCamp: allMeetup(
+      filter: { title: { regex: "/FreeCodeCamp/" } }
+      limit: 1
+    ) {
+      edges {
+        node {
+          id
+          title
+          description
+          location
+          start
+          end
+          calendarId
+        }
+      }
+    }
+  }
+`
