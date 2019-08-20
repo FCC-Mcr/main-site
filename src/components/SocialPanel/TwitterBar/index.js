c81582e66f03b212bbd5867f2f314b638149b2a5
Adam Collier <adamcollierart@gmail.com>
2019-08-20 19:50:39 +0100

    feat: link tag instead of div, style module added

--- src/components/SocialPanel/TwitterBar/index.js
+++ src/components/SocialPanel/TwitterBar/index.js
@@ -1,7 +1,17 @@
 import React from "react"
 
+import arrow from "../../../icons/arrow-white.svg"
+
 import styles from "./twitterBar.module.scss"
 
-const TwitterBar = () => <div>Follow us on Twitter</div>
+const TwitterBar = () => (
+  <a
+    href="https://twitter.com/FreeCodeCampMCR"
+    className={`${styles.twitterBar} u-shadow--3`}
+  >
+    Follow us on Twitter
+    <img src={arrow} alt="" />
+  </a>
+)
 
 export default TwitterBar
