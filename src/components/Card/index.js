bb2228cb549081e780a4853e75d79d69cac85285
Adam Collier <adamcollierart@gmail.com>
2019-08-20 19:37:31 +0100

    chore: removed padding prop

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,19 +5,13 @@ import styles from "./card.module.scss"
  * @param { string } className class to add to className string
  * @param { Number } height determines box-shadow strength
  */
-const Card = ({
-  children,
-  className,
-  height = 0,
-  padding = 1.1,
-  borderRadius = 10,
-}) => {
+const Card = ({ children, className, height = 0, borderRadius = 10 }) => {
   const shadowClass = height > 0 ? `u-shadow--${height}` : ""
 
   return (
     <div
       className={`${styles.card} ${className} ${shadowClass}`}
-      style={{ padding: `${padding}rem`, borderRadius: `${borderRadius}px` }}
+      style={{ borderRadius: `${borderRadius}px` }}
     >
       {children}
     </div>
