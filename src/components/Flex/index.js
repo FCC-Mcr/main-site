f3571bb20f73347b41212d8f0ba7cbbda038e505
Adam Collier <adamcollierart@gmail.com>
2019-08-20 21:21:23 +0100

    chore: fix rename

--- src/components/Flex/index.js
+++ src/components/Flex/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import styles from "./flex.module.scss"
 
 const index = ({ children }) => {
-  return <div className={`${styles.grid} center px-1`}>{children}</div>
+  return <div className={`${styles.flex} center px-1`}>{children}</div>
 }
 
 export default index
