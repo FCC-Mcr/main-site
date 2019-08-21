4bbde6a358f8ccca282af5242242197aaadf637e
anthonytranDev <anthonytran.dev@gmail.com>
2019-08-21 22:34:10 +0100

    feat: rm unnecessary comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,11 +58,8 @@ module.exports = {
         background_color: `#4D844F`,
         theme_color: `#4D844F`,
         display: `minimal-ui`,
-        icon: `src/images/site-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/site-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
