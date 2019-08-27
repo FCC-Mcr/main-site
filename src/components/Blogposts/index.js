2f4f08214eab0f0b226b004ca827bd87ab11628e
Adam Collier <adamcollierart@gmail.com>
2019-08-27 18:09:00 +0100

    feat: blogpost component

---
+++ src/components/Blogposts/index.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { Link } from "gatsby"
+import styles from "./blogposts.module.scss"
+
+import arrow from "../../icons/arrow.svg"
+
+const index = ({ data, title, flex }) => (
+  <div className={`${styles.blogposts} ${flex ? styles.flex : ""}`}>
+    <h1>{title}</h1>
+    {data.map(({ node }) => (
+      <div className={styles.blogPost} key={node.id}>
+        <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+          <h3>{node.frontmatter.title}</h3>
+        </Link>
+        <p>{node.excerpt}</p>
+        <small>Written by {node.frontmatter.author}</small>
+        <small>{node.timeToRead} min read</small>
+      </div>
+    ))}
+    {flex && (
+      <Link to="/blog/">
+        More blog posts <img src={arrow} alt="arrow icon" />
+      </Link>
+    )}
+  </div>
+)
+
+export default index
