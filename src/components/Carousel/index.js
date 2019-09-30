e3dee4f790db945ec9f50d13e76ac185b4a8ca49
Adam <adamcollierart@gmail.com>
2019-09-30 20:56:15 +0100

    style: hide carousel using wrapper div (#68)
    * style: hide carousel using wrapper div
    
    * style: reduce height

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -2,7 +2,11 @@ import React from "react"
 import styles from "./carousel.module.scss"
 
 const index = ({ children }) => {
-  return <div className={`${styles.carousel} full-width`}>{children}</div>
+  return (
+    <div className={`${styles.carouselWrapper} full-width`}>
+      <div className={`${styles.carousel} full-width`}>{children}</div>
+    </div>
+  )
 }
 
 export default index
