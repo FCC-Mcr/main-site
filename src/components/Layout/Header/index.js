c6c2897d36f451908a852ac7e23981f306a177b8
Adam <adamcollierart@gmail.com>
2019-10-04 11:20:55 +0100

    Fab/initial setup (#70)
    * feat: add FAB component
    
    * fix: remove console log
    
    * style: changes from white to green
    
    * fix: add start and end filter
    
    * fix: remove console log
    
    * feat: created FAB component
    
    * fix: click handled correctly for background
    
    * chore: updated navigation links
    
    * chore: removed forum, changed div to nav tag

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -2,6 +2,8 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import FAB from "../../FAB"
+
 // icons
 import logo from "../../../icons/logo.svg"
 
@@ -9,29 +11,29 @@ import styles from "./header.module.scss"
 
 const MainNav = () => (
   <nav>
-    <a href="https://www.freecodecamp.org/forum/c/united-kingdom-study-groups/manchester">
-      Forum
-    </a>
-    <Link to="/">Organisers</Link>
+    <Link to="/">Home</Link>
     <Link to="/blog/">Blog</Link>
-    <Link to="/learning-resources/">Learning Resources</Link>
+    <Link to="/learning-resources/">Resources</Link>
+    <Link to="/upcoming-talks/">Upcoming Talks</Link>
   </nav>
 )
 
 const Header = ({ siteTitle }) => (
-  <header className={`${styles.header}  u-shadow--2`}>
-    <h2>
-      <Link to="/" className={styles.title}>
-        {/* need to bring in icon */}
-        <span className={styles.logo}>
-          <img src={logo} alt="" />
-        </span>
-        {siteTitle}
-      </Link>
-    </h2>
-
-    <MainNav />
-  </header>
+  <>
+    <header className={`${styles.header}  u-shadow--2`}>
+      <h2>
+        <Link to="/" className={styles.title}>
+          {/* need to bring in icon */}
+          <span className={styles.logo}>
+            <img src={logo} alt="" />
+          </span>
+          {siteTitle}
+        </Link>
+      </h2>
+      <MainNav />
+    </header>
+    <FAB />
+  </>
 )
 
 Header.propTypes = {
