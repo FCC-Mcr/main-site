ea64b814750b9a36657a37e4dec25b6eba42ddda
Adam Collier <adamcollierart@gmail.com>
2019-10-06 12:10:24 +0100

    chore: all images and twitter links added

--- src/components/Organisers/index.js
+++ src/components/Organisers/index.js
@@ -10,31 +10,23 @@ const index = () => {
   const data = useStaticQuery(graphql`
     query {
       adam: file(relativePath: { eq: "organiser-adam.jpg" }) {
-        childImageSharp {
-          fixed(width: 70, height: 70) {
-            ...GatsbyImageSharpFixed
-          }
-        }
+        ...organiserImage
       }
       james: file(relativePath: { eq: "organiser-james.jpg" }) {
-        childImageSharp {
-          fixed(width: 70, height: 70) {
-            ...GatsbyImageSharpFixed
-          }
-        }
+        ...organiserImage
       }
-      pete: file(relativePath: { eq: "organiser-adam.jpg" }) {
-        childImageSharp {
-          fixed(width: 70, height: 70) {
-            ...GatsbyImageSharpFixed
-          }
-        }
+      pete: file(relativePath: { eq: "organiser-pete.jpg" }) {
+        ...organiserImage
       }
       fey: file(relativePath: { eq: "organiser-fey.jpg" }) {
-        childImageSharp {
-          fixed(width: 70, height: 70) {
-            ...GatsbyImageSharpFixed
-          }
+        ...organiserImage
+      }
+    }
+
+    fragment organiserImage on File {
+      childImageSharp {
+        fluid(maxWidth: 70, maxHeight: 70, quality: 80) {
+          ...GatsbyImageSharpFluid
         }
       }
     }
@@ -46,7 +38,7 @@ const index = () => {
       <Carousel>
         <Card height={3} className={`${styles.organiser} p-1`}>
           <div className={styles.grid}>
-            <Img fixed={data.james.childImageSharp.fixed} />
+            <Img fluid={data.james.childImageSharp.fluid} />
             <div>
               <h3>James Davenport</h3>
               <a href="https://twitter.com/JD_aka_Techy">@JD_aka_Techy</a>
@@ -60,7 +52,7 @@ const index = () => {
         </Card>
         <Card height={3} className={`${styles.organiser} p-1`}>
           <div className={styles.grid}>
-            <Img fixed={data.adam.childImageSharp.fixed} />
+            <Img fluid={data.adam.childImageSharp.fluid} />
             <div>
               <h3>Adam Collier</h3>
               <a href="https://twitter.com/CollierAdam">@collieradam</a>
@@ -74,7 +66,7 @@ const index = () => {
         </Card>
         <Card height={3} className={`${styles.organiser} p-1`}>
           <div className={styles.grid}>
-            <Img fixed={data.james.childImageSharp.fixed} />
+            <Img fluid={data.pete.childImageSharp.fluid} />
             <div>
               <h3>Pete Daily</h3>
               <a href="https://twitter.com/peterdaily">@peterdaily</a>
@@ -88,7 +80,7 @@ const index = () => {
         </Card>
         <Card height={3} className={`${styles.organiser} p-1`}>
           <div className={styles.grid}>
-            <Img fixed={data.fey.childImageSharp.fixed} />
+            <Img fluid={data.fey.childImageSharp.fluid} />
             <div>
               <h3>Fey Ijaware</h3>
               <a href="https://twitter.com/feyagape">@feyagape</a>
