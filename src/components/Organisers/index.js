858a913613882a9baf54b51a018b753074874893
Adam <adamcollierart@gmail.com>
2019-10-06 12:17:05 +0100

    Organisers/organiser info (#71)
    * feat: added basic info
    
    * chore: all images and twitter links added

--- src/components/Organisers/index.js
+++ src/components/Organisers/index.js
@@ -10,24 +10,23 @@ const index = () => {
   const data = useStaticQuery(graphql`
     query {
       adam: file(relativePath: { eq: "organiser-adam.jpg" }) {
-        childImageSharp {
-          fixed(width: 70, height: 70) {
-            ...GatsbyImageSharpFixed
-          }
-        }
+        ...organiserImage
       }
-      james: file(relativePath: { eq: "organiser-adam.jpg" }) {
-        childImageSharp {
-          fixed(width: 70, height: 70) {
-            ...GatsbyImageSharpFixed
-          }
-        }
+      james: file(relativePath: { eq: "organiser-james.jpg" }) {
+        ...organiserImage
       }
-      pete: file(relativePath: { eq: "organiser-adam.jpg" }) {
-        childImageSharp {
-          fixed(width: 70, height: 70) {
-            ...GatsbyImageSharpFixed
-          }
+      pete: file(relativePath: { eq: "organiser-pete.jpg" }) {
+        ...organiserImage
+      }
+      fey: file(relativePath: { eq: "organiser-fey.jpg" }) {
+        ...organiserImage
+      }
+    }
+
+    fragment organiserImage on File {
+      childImageSharp {
+        fluid(maxWidth: 70, maxHeight: 70, quality: 80) {
+          ...GatsbyImageSharpFluid
         }
       }
     }
@@ -39,9 +38,23 @@ const index = () => {
       <Carousel>
         <Card height={3} className={`${styles.organiser} p-1`}>
           <div className={styles.grid}>
-            <Img fixed={data.james.childImageSharp.fixed} />
+            <Img fluid={data.james.childImageSharp.fluid} />
             <div>
               <h3>James Davenport</h3>
+              <a href="https://twitter.com/JD_aka_Techy">@JD_aka_Techy</a>
+            </div>
+            <p>
+              “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam.”
+            </p>
+          </div>
+        </Card>
+        <Card height={3} className={`${styles.organiser} p-1`}>
+          <div className={styles.grid}>
+            <Img fluid={data.adam.childImageSharp.fluid} />
+            <div>
+              <h3>Adam Collier</h3>
               <a href="https://twitter.com/CollierAdam">@collieradam</a>
             </div>
             <p>
@@ -53,10 +66,10 @@ const index = () => {
         </Card>
         <Card height={3} className={`${styles.organiser} p-1`}>
           <div className={styles.grid}>
-            <Img fixed={data.james.childImageSharp.fixed} />
+            <Img fluid={data.pete.childImageSharp.fluid} />
             <div>
-              <h3>James Davenport</h3>
-              <a href="https://twitter.com/CollierAdam">@collieradam</a>
+              <h3>Pete Daily</h3>
+              <a href="https://twitter.com/peterdaily">@peterdaily</a>
             </div>
             <p>
               “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -67,10 +80,10 @@ const index = () => {
         </Card>
         <Card height={3} className={`${styles.organiser} p-1`}>
           <div className={styles.grid}>
-            <Img fixed={data.james.childImageSharp.fixed} />
+            <Img fluid={data.fey.childImageSharp.fluid} />
             <div>
-              <h3>James Davenport</h3>
-              <a href="https://twitter.com/CollierAdam">@collieradam</a>
+              <h3>Fey Ijaware</h3>
+              <a href="https://twitter.com/feyagape">@feyagape</a>
             </div>
             <p>
               “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
