7852f1a22d48650da44949e5f1d1cfae939fb570
Adam Collier <adamcollierart@gmail.com>
2019-11-21 21:02:04 +0000

    feat: reuse LearningResources component

--- src/pages/learning-resources.js
+++ src/pages/learning-resources.js
@@ -1,23 +1,20 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
+import LearningResources from "../components/LearningResources"
+import styles from "./learning-resources.module.scss"
 
 import Layout from "../components/Layout"
+import Center from "../components/Center"
 import SEO from "../components/SEO"
 
 const LearningResourcesPage = ({ data }) => (
   <Layout>
     <SEO title="Learning Resources" />
-    <article className="main blog">
-      <h1>Learning Resources</h1>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-            <h3>{node.frontmatter.title}</h3>
-          </Link>
-          <p>{node.frontmatter.description}</p>
-        </div>
-      ))}
-    </article>
+    <Center>
+      <article className={styles.learningResources}>
+        <LearningResources />
+      </article>
+    </Center>
   </Layout>
 )
 
