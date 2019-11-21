a7e3edb490eca9827e7abca6e67cf83510102a00
Adam Collier <adamcollierart@gmail.com>
2019-11-21 21:13:21 +0000

    feat: component to show logo depending on prop

---
+++ src/components/Logo/index.js
@@ -0,0 +1,44 @@
+import React from "react"
+
+import accessibilityLogo from "../../../logos/accessibility.svg"
+import javascriptLogo from "../../../logos/javascript.svg"
+import cssLogo from "../../../logos/css.svg"
+import nodeLogo from "../../../logos/node.svg"
+import reactLogo from "../../../logos/react.svg"
+import testingLogo from "../../../logos/testing.svg"
+import toolbeltLogo from "../../../logos/toolbelt.svg"
+
+const index = ({ name }) => {
+  let logo = name => {
+    switch (name) {
+      case "accessibility": {
+        return accessibilityLogo
+      }
+      case "node": {
+        return nodeLogo
+      }
+      case "testing": {
+        return testingLogo
+      }
+      case "toolbelt": {
+        return toolbeltLogo
+      }
+      case "javascript": {
+        return javascriptLogo
+      }
+      case "react": {
+        return reactLogo
+      }
+      case "css": {
+        return cssLogo
+      }
+      default: {
+        return console.log("oh no there's no resources")
+      }
+    }
+  }
+
+  return <img src={logo(name)} alt={`${name} logo`} />
+}
+
+export default index
