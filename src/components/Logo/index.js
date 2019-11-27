4e084c73b18674ac9cbc7d69644850cc41f9eab9
Adam Collier <adamcollierart@gmail.com>
2019-11-27 18:46:11 +0000

    feat: angular and css logo added

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,18 +1,34 @@
 import React from "react"
 
 import accessibilityLogo from "../../../logos/accessibility.svg"
+import angularLogo from "../../../logos/angular.svg"
 import javascriptLogo from "../../../logos/javascript.svg"
 import cssLogo from "../../../logos/css.svg"
+import htmlLogo from "../../../logos/html.svg"
 import nodeLogo from "../../../logos/node.svg"
 import reactLogo from "../../../logos/react.svg"
 import testingLogo from "../../../logos/testing.svg"
 import toolbeltLogo from "../../../logos/toolbelt.svg"
+import meetupLogo from "../../../logos/meetup.svg"
+import eventbriteLogo from "../../../logos/eventbrite.svg"
 
 const Index = ({ name }) => {
   let logo = name => {
     switch (name) {
+      case "angular": {
+        return angularLogo
+      }
       case "accessibility": {
         return accessibilityLogo
+      }
+      case "eventbrite": {
+        return eventbriteLogo
+      }
+      case "html": {
+        return htmlLogo
+      }
+      case "meetup": {
+        return meetupLogo
       }
       case "node": {
         return nodeLogo
