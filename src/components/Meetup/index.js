7a0959a49f24c16f9b51c2f862f2406aa777a36e
Adam Collier <adamcollierart@gmail.com>
2019-11-27 18:47:16 +0000

    chore: simplified styles, show eventbrite and meetup logos

--- src/components/Meetup/index.js
+++ src/components/Meetup/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import urlRegex from "url-regex"
+import Logo from "../Logo"
 import Button from "../Button"
+
+// import meetupLogo from "../../../logos/meetup.svg"
+// import eventbriteLogo from "../../../logos/eventbrite.svg"
 
 import downloadCalendarFile from "../../utils/downloadCalendarFile"
 import styles from "./meetup.module.scss"
@@ -16,17 +21,56 @@ const Index = ({ node }) => {
     month: "long",
   }
 
+  let eventLink = {}
+
+  let meetupLinks = node.description.match(urlRegex())
+
+  meetupLinks &&
+    meetupLinks.forEach(link => {
+      if (link.includes("meetup") && link.indexOf("<") === -1) {
+        eventLink.title = "meetup"
+        eventLink.url = link
+        eventLink.color = "var(--meetup-color)"
+      }
+      if (link.includes("eventbrite") && link.indexOf("<") === -1) {
+        eventLink.title = "eventbrite"
+        eventLink.url = link
+        eventLink.color = "var(--eventbrite-color)"
+      }
+    })
+
   // remove any <br> tags at the beginning of the string
   node.excerpt = node.excerpt
     .replace(/<br\s*\/?>/gi, " ")
     .replace(/<b>|<\/b>/gm, "")
     .replace(/<a\s+/gi, `<a rel="noopener" `)
 
+  const MeetupLink = () => {
+    if (Object.entries(eventLink).length !== 0) {
+      let { color, title, url } = eventLink
+      return (
+        <a href={url} className={styles.link}>
+          {/* <img src={eventbriteLogo} alt="" /> */}
+          <Logo name={title} />
+          <Button color={color} text={title} />
+        </a>
+      )
+    } else {
+      return (
+        <Button
+          color="#D43535"
+          text="Add to Calendar"
+          onClick={() => downloadCalendarFile(node)}
+        />
+      )
+    }
+  }
+
   return (
     <div className={styles.meetup}>
       <span>{date.toLocaleDateString("en-GB", options)}</span>
       <h3>{node.title}</h3>
-      <div>
+      <div className={styles.details}>
         <img src={clock} alt="clock icon" />
         <span>
           {node.start.match(/\d\d:\d\d/)} - {node.end.match(/\d\d:\d\d/)}
@@ -35,11 +79,9 @@ const Index = ({ node }) => {
         <span className={styles.location}>{node.location}</span>
       </div>
       <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-      <Button
-        color="#D43535"
-        text="Add to Calendar"
-        onClick={() => downloadCalendarFile(node)}
-      />
+      <div className={styles.link}>
+        <MeetupLink />
+      </div>
     </div>
   )
 }
